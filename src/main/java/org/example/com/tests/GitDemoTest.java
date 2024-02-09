ac201485e722bd50f2211347d5e406b5b73436b7
AkshayShadakshari3009 <AkshayShadakshari@dckap.com>
2024-02-09 21:49:05 +0530

    Initial commit as commit message is fine

---
+++ src/main/java/org/example/com/tests/GitDemoTest.java
@@ -0,0 +1,12 @@
+package org.example.com.tests;
+
+import static java.lang.System.*;
+
+public class GitDemoTest {
+
+
+    public static void main(String[] args) {
+        out.println("Dummy Description");
+    }
+
+}
